7616a87deab6a223ae3a42a73238b779c668fe5c
mahderf <mymahder@gmail.com>
2017-08-25 19:30:27 -0400

    added a logout link

--- src/main/java/roboresume/mahi/roboresume/controller/MainController.java
+++ src/main/java/roboresume/mahi/roboresume/controller/MainController.java
@@ -1,6 +1,9 @@
 package roboresume.mahi.roboresume.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -17,6 +20,8 @@ import roboresume.mahi.roboresume.repository.ResumeRepository;
 import roboresume.mahi.roboresume.repository.SkillsRepository;
 import roboresume.mahi.roboresume.repository.WorkRepository;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.util.ArrayList;
 
@@ -203,6 +208,15 @@ public class MainController {
     @GetMapping("/login")
     public String logon() {
         return "login";
+    }
+
+    @GetMapping("/logout")
+    public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/login";
     }
     @GetMapping("/welcome")
     public String WelcomeAfterLogin() {
